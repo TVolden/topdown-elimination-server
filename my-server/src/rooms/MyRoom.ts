8fdc771a3a6cf6ca91f1018eb0c9afce6711aa73
Thomas Volden <thvo@itu.dk>
2024-08-25 23:53:29 +0200

    Setup for server

---
+++ my-server/src/rooms/MyRoom.ts
@@ -0,0 +1,38 @@
+import { Room, Client } from "@colyseus/core";
+import { MyRoomState, Player } from "./schema/MyRoomState";
+
+export class MyRoom extends Room<MyRoomState> {
+  maxClients = 4;
+
+  onCreate (options: any) {
+    this.setState(new MyRoomState());
+
+    this.onMessage("move", (client, message) => {
+      var char = this.state.players.get(client.sessionId)
+      char.x += message.x;
+      char.y += message.y;
+    });
+  }
+
+  onJoin (client: Client, options: any) {
+    console.log(client.sessionId, "joined!");
+
+    const player = new Player();
+    const FLOOR_SIZE = 4;
+    player.x = -(FLOOR_SIZE/2) + (Math.random() * FLOOR_SIZE);
+    player.y = 1.031;
+
+    this.state.players.set(client.sessionId, player);
+  }
+
+  onLeave (client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!");
+
+    this.state.players.delete(client.sessionId);
+  }
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...");
+  }
+
+}
